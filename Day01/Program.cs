ac59e8a3fe73701ba70984274731945e5b6b9f40
Lukas Dürrenberger <lukas.duerrenberger@zuehlke.com>
2020-12-01 17:07:59 +0100

    Add project structure and solution for Day01

---
+++ Day01/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day01
+{
+    public static class Program
+    {
+        private const int Target = 2020;
+
+        public static void Main()
+        {
+            var input = File.ReadLines("input.txt")
+                            .Select(s => Convert.ToInt32(s))
+                            .OrderBy(i => i)
+                            .ToList();
+
+            PartOne(input);
+            PartTwo(input);
+        }
+
+        private static void PartOne(List<int> input)
+        {
+            var (number, result) = input.Where(line => line * 2 <= Target)
+                                        .Select(line => (
+                                                            number: line,
+                                                            result: input.BinarySearch(Target - line)
+                                                        ))
+                                        .Single(search => search.result >= 0);
+
+            Console.WriteLine($"{number} * {input[result]} = {number * input[result]}");
+        }
+
+        private static void PartTwo(List<int> input)
+        {
+            var (numberOne, numberTwo, result) = input.SelectMany(_ => input,
+                                                                  (firstNumber, secondNumber) => (
+                                                                          numberOne: firstNumber,
+                                                                          numberTwo: secondNumber,
+                                                                          result: input.BinarySearch(Target -
+                                                                              firstNumber - secondNumber)
+                                                                      )
+                                                                 )
+                                                      .First(search => search.result >= 0);
+
+            Console.WriteLine($"{numberOne} * {numberTwo} * {input[result]} = {numberOne * numberTwo * input[result]}");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Advent of Code 2020
+
+First time joining the programming puzzle solving during Advent.
+
+**Note:** It's more fun to solve these puzzles yourself, so I highly recommend to do so, before checking my solutions.
+
+- Day01 - [Puzzle](https://adventofcode.com/2020/day/1) - [Solution](Day01/)
+
+## License
+
+The input files and puzzle text are under copyright of Advent of Code and are only included for completness sake.
+
+As for the code see the [license file](LICENSE.md).
