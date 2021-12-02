583aff24e1f46b3420484d541666360374c70f8a
Lukas Dürrenberger <lukas.duerrenberger@zuehlke.com>
2021-12-02 10:47:55 +0100

    Add solution for Day02

---
+++ 2021/Day02/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+var inputs = File.ReadLines("input.txt")
+                 .Select(l =>
+                         {
+                             var instruction = l.Split(' ');
+                             return new
+                                    {
+                                        Instruction = instruction[0],
+                                        Amount = int.Parse(instruction[1])
+                                    };
+                         })
+                 .ToList();
+
+var depth = inputs.Where(i => i.Instruction == "up")
+                  .Select(i => -i.Amount)
+                  .Concat(inputs.Where(i => i.Instruction == "down")
+                               .Select(i => i.Amount))
+                  .Sum();
+var forward = inputs.Where(i => i.Instruction == "forward")
+                    .Select(i => i.Amount)
+                    .Sum();
+Console.WriteLine($"PartOne: {depth * forward}");
+
+var aim = 0;
+var y = 0;
+var x = 0;
+
+foreach (var instruction in inputs)
+{
+    aim += instruction.Instruction switch
+           {
+               "down" => instruction.Amount,
+               "up" => -instruction.Amount,
+               _ => 0
+           };
+
+    if (instruction.Instruction == "forward")
+    {
+        x += instruction.Amount;
+        y += aim * instruction.Amount;
+    }
+}
+
+Console.WriteLine($"PartTwo: {x * y}");
--- 2021/README.md
+++ 2021/README.md
@@ -6,6 +6,7 @@ I'm hoping this year won't have a deal breaker puzzle as last year.
 **Note:** It's more fun to solve these puzzles yourself, so I highly recommend to do so, before checking my solutions.
 
 - Day01 - [Puzzle](https://adventofcode.com/2021/day/1) - [Solution](Day01/)
+- Day01 - [Puzzle](https://adventofcode.com/2021/day/2) - [Solution](Day02/)
 
 ## Community
 
@@ -13,6 +14,8 @@ Looking forward to discussing things on the [SFML Discord](https://discord.gg/nr
 In case anyone wants to checkout some other solutions, here are links to some other repos.
 
 - [Dew](https://github.com/crumblingstatue/advent-of-code)
+- [Jonny](https://github.com/JonnyPtn/AOC)
 - [Mario](https://github.com/MarioLiebisch/Advent-of-Code-2021)
+- [metaquarx](https://gitlab.com/metaquarx/aoc)
 - [nick-kline](https://github.com/nick-kline/AOC2021)
 - [Stritch](https://github.com/MetGang/Advent-of-Code)
