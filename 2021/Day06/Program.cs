bbc70e93b4e19d85d5156d30765928be945a405c
Lukas Dürrenberger <lukas.duerrenberger@zuehlke.com>
2021-12-06 08:08:20 +0100

    Add solution for Day06

---
+++ 2021/Day06/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+var input = File.ReadAllLines("input.txt")
+                .First();
+
+var partOne = input.Split(',')
+                   .Select(int.Parse)
+                   .ToList();
+
+for (var day = 0; day < 80; day++)
+{
+    var today = partOne.Count;
+
+    for (var fish = 0; fish < today; fish++)
+    {
+        if (partOne[fish] == 0)
+        {
+            partOne[fish] = 6;
+            partOne.Add(8);
+        }
+        else
+        {
+            partOne[fish] -= 1;
+        }
+    }
+}
+
+Console.WriteLine($"PartOne: {partOne.Count}");
+
+var partTwo = input.Split(',')
+                   .Select(int.Parse)
+                   .ToList();
+
+var schoolOfFish = new List<long>
+                   {
+                       0, 0, 0, 0, 0, 0, 0, 0, 0
+                   };
+
+foreach (var fish in partTwo)
+{
+    schoolOfFish[fish]++;
+}
+
+for (var day = 0; day < 256; day++)
+{
+    var first = schoolOfFish[0];
+    schoolOfFish.RemoveAt(0);
+    schoolOfFish[6] += first;
+    schoolOfFish.Add(first);
+}
+
+Console.Write($"PartTwo: {schoolOfFish.Sum()}");
