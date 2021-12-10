4c78a047182f67ace6691bfcf84845177798a122
Lukas Dürrenberger <lukas.duerrenberger@zuehlke.com>
2021-12-10 21:52:10 +0100

    Add solution for Day10

---
+++ 2021/Day10/Program.cs
@@ -0,0 +1,64 @@
+var input = File.ReadAllLines("input.txt")
+                .Where(l => l.Length > 0);
+var syntaxPoints = new Dictionary<char, int>
+             {
+                 { ')', 3 },
+                 { ']', 57 },
+                 { '}', 1197 },
+                 { '>', 25137 }
+             };
+var completionPoints = new Dictionary<char, long>
+                   {
+                       { ')', 1 },
+                       { ']', 2 },
+                       { '}', 3 },
+                       { '>', 4 }
+                   };
+var mapping = new Dictionary<char, char>
+              {
+                  { '(', ')' },
+                  { '[', ']' },
+                  { '{', '}' },
+                  { '<', '>' }
+              };
+
+var partOne = 0;
+var scores = new List<long>();
+
+foreach (var line in input)
+{
+    var stack = new Stack<char>();
+
+    foreach (var character in line)
+    {
+        if (character is '(' or '[' or '<' or '{')
+        {
+            stack.Push(character);
+        }
+        else if (character is ')' or ']' or '>' or '}')
+        {
+            // Corrupt Line
+            if (stack.Any() && mapping[stack.Pop()] != character)
+            {
+                partOne += syntaxPoints[character];
+                stack.Clear();
+                break;
+            }
+        }
+    }
+
+    // Incomplete Line
+    if (stack.Any())
+    {
+        scores.Add(stack.Aggregate(0L, (current, liner) => current * 5L + completionPoints[mapping[liner]]));
+    }
+}
+
+var partTwo = scores.OrderBy(s => s)
+                    .Skip(scores.Count / 2)
+                    .Take(1)
+                    .Single();
+                    
+
+Console.WriteLine($"PartOne: {partOne}");
+Console.WriteLine($"PartTwo: {partTwo}");
--- 2021/README.md
+++ 2021/README.md
@@ -14,6 +14,7 @@ I'm hoping this year won't have a deal breaker puzzle as last year.
 - Day07 - [Puzzle](https://adventofcode.com/2021/day/7) - [Solution](Day07/)
 - Day08 - [Puzzle](https://adventofcode.com/2021/day/8) - [Solution](Day08/)
 - Day09 - [Puzzle](https://adventofcode.com/2021/day/9) - [Solution](Day09/)
+- Day10 - [Puzzle](https://adventofcode.com/2021/day/10) - [Solution](Day10/)
 
 ## Community
 
