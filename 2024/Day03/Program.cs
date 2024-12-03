f5544c6a995927acddaf7554d2bf356f448f6c3b
Lukas Dürrenberger <eXpl0it3r@sfml-dev.org>
2024-12-03 19:24:17 +0100

    Use original input for Day 03

--- 2024/Day03/Program.cs
+++ 2024/Day03/Program.cs
@@ -1,27 +1,25 @@
 using System.Text.RegularExpressions;
 
-var input = await File.ReadAllLinesAsync("input.txt");
+var input = await File.ReadAllTextAsync("input.txt");
 
-var resultPart1 = input.Select(line => Regex.Matches(line, @"mul\((\d{1,3}),(\d{1,3})\)")
-                                            .Select(m => (int.Parse(m.Groups[1].Value), int.Parse(m.Groups[2].Value))))
-                       .Select(values => values.Sum(v => v.Item1 * v.Item2))
-                       .Sum();
+var resultPart1 = Regex.Matches(input, @"mul\((\d{1,3}),(\d{1,3})\)")
+                       .Select(m => (int.Parse(m.Groups[1].Value), int.Parse(m.Groups[2].Value)))
+                       .Sum(v => v.Item1 * v.Item2);
 
-var resultPart2 = input.Select(line => Regex.Matches(line, @"mul\((\d{1,3}),(\d{1,3})\)")
-                                            .Select(m =>
-                                                    {
-                                                        var disable = line.LastIndexOf("don't()", m.Index, StringComparison.InvariantCulture);
-                                                        var enable = line.LastIndexOf("do()", m.Index, StringComparison.InvariantCulture);
+var resultPart2 = Regex.Matches(input, @"mul\((\d{1,3}),(\d{1,3})\)")
+                       .Select(m =>
+                               {
+                                   var disable = input.LastIndexOf("don't()", m.Index, StringComparison.InvariantCulture);
+                                   var enable = input.LastIndexOf("do()", m.Index, StringComparison.InvariantCulture);
 
-                                                        if (disable != -1 && disable > enable)
-                                                        {
-                                                            return (0, 0);
-                                                        }
+                                   if (disable != -1 && disable > enable)
+                                   {
+                                       return (0, 0);
+                                   }
 
-                                                        return (int.Parse(m.Groups[1].Value), int.Parse(m.Groups[2].Value));
-                                                    }))
-                       .Select(values => values.Sum(v => v.Item1 * v.Item2))
-                       .Sum();
+                                   return (int.Parse(m.Groups[1].Value), int.Parse(m.Groups[2].Value));
+                               })
+                       .Sum(v => v.Item1 * v.Item2);
 
 Console.WriteLine($"Part 1: {resultPart1}");
 Console.WriteLine($"Part 2: {resultPart2}");
